281fc04e7e977c8c5d6896bce3bf41428b54430f
Marcos Reis <marcos.lourenco.reis@gmail.com>
2021-04-09 18:13:37 -0300

    atualizacao

---
+++ Explicacoes/aprendendoJS.js
@@ -0,0 +1,14 @@
+// Formas de selecionar os elementos:
+
+//Importante repeitar letras maisculas e minusculas.
+
+// document.getElementByID("nome do ID")    Seleciona pelo ID
+// document.getElementsByClassName("nome da class")   Seleciona pela Class
+// document.getElementsByTagName("nome da tag")       Seleciona atraves da TAG
+// document.getElementsByName("Se usa o name") por exemplo: input name=Esse name que ele busca
+
+//para fazer alteração, se da o seguinte codigo:
+// documents.getElementsClassName("nome da class")[0].innerHTML = "escreva a alteração";
+
+//No codigo acima o q vai dentro de [ ] é o numero do elemento, contagem começa em 0, e após o innerHTML = " " - o que for colocado dentro de " " é que vai vir a substituir o elemento selecionado.
+
