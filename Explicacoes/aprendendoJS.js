bf88a8573ad8b9175d11b6c36a3fe696b28ce91b
Marcos Reis <marcos.lourenco.reis@gmail.com>
2021-04-10 01:06:55 -0300

    atualização

--- Explicacoes/aprendendoJS.js
+++ Explicacoes/aprendendoJS.js
@@ -12,3 +12,25 @@
 
 //No codigo acima o q vai dentro de [ ] é o numero do elemento, contagem começa em 0, e após o innerHTML = " " - o que for colocado dentro de " " é que vai vir a substituir o elemento selecionado.
 
+
+// document.querySelector(" ") com query podemos selecionar IDs, Classes. basta colocar . ou #
+// document.querySelectorAll (" ") Seleciona todos os elementos do seguimento selecionado.
+
+function green() {
+    document.querySelector("#container").classList.remove("azul");
+    document.querySelector("#container").classList.remove("vermelho"); 
+    document.querySelector('#container').classList.add('verde');
+}
+
+function red() {
+    document.querySelector("#container").classList.remove("azul");
+    document.querySelector("#container").classList.remove("verde");
+    document.querySelector("#container").classList.add("vermelho");
+}
+
+function blue() {
+    document.querySelector("#container").classList.remove("verde");
+    document.querySelector("#container").classList.remove("vermelho");
+    document.querySelector("#container").classList.add("azul");
+}
+
