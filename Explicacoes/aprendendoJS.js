45804f0df42d91b2f974964d0a876aa1d2489783
Marcos Reis <marcos.lourenco.reis@gmail.com>
2021-05-03 23:37:51 -0300

    ATUALIZANDO

--- Explicacoes/aprendendoJS.js
+++ Explicacoes/aprendendoJS.js
@@ -585,4 +585,35 @@ temp.then(function(resultado){
 
 console.log(temp) 
 
-// Fletch em JS
+// Fetch em JS
+
+function LoadPosts() {
+    document.getElementById("posts").innerHTML = "carregando";
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function (resultado){
+            return resultado.json();
+        })
+
+        .then(function(json){
+            montarblog(json);
+        })
+
+        .catch(function(eror){
+            console.log("Deu problema!");
+        });
+}
+
+    function montarblog(lista){
+        let html = "";
+
+        for(let i in lista){
+            html += "<h3>"+lista[i].title+"</h3>";
+            html += lista[i].body+"<br/>"
+            html += "<hr/>"
+        }
+
+        document.getElementById("posts").innerHTML = html;
+
+
+    }
