c8005c148301516e5c9625bf1f160cca79755897
Marcos Reis <marcos.lourenco.reis@gmail.com>
2021-05-11 23:09:34 -0300

    atualizando

--- projetoNode/server.js
+++ projetoNode/server.js
@@ -1,6 +1,18 @@
 const app = require('./app');
 
-app.set('port', 7777)
+const mongoose = require('mongoose');
+
+require('dotenv').config({path:'variables.env'})
+
+//Conexao ao Banco de dados
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error',(error)=> {
+    console.error("ERRO: "+error.message);
+})
+
+app.set('port', process.env.PORT || 7777)
 const server = app.listen(app.get('port'), () =>{
     console.log("Servidor rodando na porta: " +server.address().port)
 });
